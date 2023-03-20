4a97ec53fd9a31ce6a4d38dc680536212ebdff5e
ajitkumarpatel1 <1ajitkumarpatel@gmail.com>
2023-03-20 13:23:44 +0530

    adding the exception.py file for error handling of project

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):
+    '''
+    This function take error and find the error details from sys and take aas input
+    and return the erro message as output
+    '''
+    _,_,exc_tb=error_detail.exc_info()                 # exc info find the values from error details 
+                                                       # and save in 3 variable but the last one is imp 
+                                                       #  because it hold the error details 
+    file_name=exc_tb.tb_frame.f_code.co_filename       # saving the error details in filename variable
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))            # Printing the error details
+
+    return error_message
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
